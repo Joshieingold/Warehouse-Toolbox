06b751723f4113a49f5b6e27090417289caf0f6c
Joshua Lynch <149664064+Joshieingold@users.noreply.github.com>
2024-10-18 12:44:05 -0300

    Cleaned up some comments

--- Import Tool.py
+++ Import Tool.py
@@ -1,7 +1,4 @@
 from pathlib import Path
-
-# from tkinter import *
-# Explicit imports to satisfy Flake8
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
